9ae1687cf3a484af2f0391b0124e3dc915be8b70
Ростислав <120830522+KAPT0SHKA@users.noreply.github.com>
2024-01-19 10:41:32 +0000

    121312

---
+++ src/Quotation.java
@@ -0,0 +1,8 @@
+public class Quotation {
+  String quote = "Кожен роботодавець отримує таких працівників, на які заслуговує.";
+  String author = "Уолтер Гілбі";
+  public void display() {
+    System.out.println(quote);
+    System.out.println("\t"+author);
+  }
+}
